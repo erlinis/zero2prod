e1c56b1e0cd50902584d2146a485ce9ff0bcb147
Erlinis Quintana <erlinis.quintana@tourlane.com>
2022-11-16 18:23:40 +0100

    Migrate from log to trace

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use actix_web::{dev::Server, web, App, HttpServer};
 use sqlx::PgPool;
+use actix_web::middleware::Logger;
 use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
@@ -9,6 +10,7 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std
 
     let server = HttpServer::new( move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection_pool.clone())
