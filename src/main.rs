413ed9d64898edbbc7df382184574d07ff55abd4
Erlinis Quintana <erlinis.quintana@tourlane.com>
2022-11-16 18:53:36 +0100

    Refactoring tracing and log to telemetry module

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use tracing_log::LogTracer;
+use zero2prod::telemetry::{get_tracing_subscriber, init_tracing_susbcriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set  tracing subscriber");
+    let subscriber = get_tracing_subscriber("zero2prod".into(), "info".into());
+    init_tracing_susbcriber(subscriber);
 
     //Panic if can;t read the configuration
     let configuration = get_configuration().expect("Failed to read configuration");
@@ -33,3 +20,5 @@ async fn main() -> std::io::Result<()> {
 
     run(listener, connection_pool)?.await
 }
+
+
